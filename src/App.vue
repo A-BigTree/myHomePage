9ff4db2656887d9632dae555b639817a1d2122b2
Shuxin-Wang <81904920+A-BigTree@users.noreply.github.com>
2023-05-15 10:28:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup></script>
+
+<template>
+  <div class="card-container">
+    <div class="header">
+      <div class="avatar">
+        <img src="https://q4.qlogo.cn/headimg_dl?dst_uin=2177742990&spec=640" alt="avatar" />
+      </div>
+      <div class="info">
+        <div class="name">一棵大树🌳</div>
+        <div class="desc">🐱热爱技术，交流分享😺</div>
+        <div class="navbar">
+          <a href="http://101.200.59.252/blog/" target="_blank" rel="noopener noreferrer">
+            <i class="fa-solid fa-newspaper"></i>个人博客
+          </a>
+          <a href="https://github.com/A-BigTree" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-github"></i>GitHub
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <i class="fa-solid fa-blog"></i>CSDN
+          </a>
+          <!--
+          <a href="https://blog.iyuan.ltd/about" target="_blank" rel="noopener noreferrer">
+            <i class="fa-solid fa-mug-saucer"></i>关于
+          </a>-->
+        </div>
+      </div>
+    </div>
+
+    <hr />
+
+    <div class="footer">
+      <div class="contact">
+        <h2>📮 联系我</h2>
+        <ul>
+          <li>Email:<a href="shuxinwang662@gmail.com">shuxinwang662@gmail.com</a></li>
+          <li>QQ:2177742990</li>
+        </ul>
+      </div>
+      <div class="project">
+        <h2>📦 项目</h2>
+        <ul>
+          <li>
+            <a href="http://101.200.59.252/seu_hotel/" target="_blank" rel="noopener noreferrer">
+                软件工程课程项目:SEU HOTEL
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/A-BigTree/hotel_Crawling" target="_blank" rel="noopener noreferrer">
+                酒店数据爬取
+            </a>
+          </li>
+          <li>
+            <a href="http://101.200.59.252/seu_hotel/" target="_blank" rel="noopener noreferrer">
+                大规模知识图谱请求API集成
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
